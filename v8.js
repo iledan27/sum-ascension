34458f807ff05c853af61fc74cad57cdba132edc
iledan27 <ile.dan27@yahoo.com>
2023-04-17 20:41:23 +0300

    v2

--- v8.js
+++ v8.js
@@ -9,40 +9,79 @@ const config = {
   },
 };
 
-const levels = [
-  {
-    buttonCounters: [4, 3, 1],
-    values: [[2], [3], [2], [2], [2], [1], [0]],
-  },
-  {
-    buttonCounters: [5, 3, 1],
-    values: [
-      [2, 3, 2, 2, 1],
-      [1, 2, 1, 3, 2],
-      [2, 2, 2, 1, 3],
-      [3, 1, 3, 2, 2],
-      [0, 2, 3, 1, 1],
-    ],
-  },
-];
+const level1 = {
+  buttonCounters: [4, 3, 1],
+  values: [[2], [3], [2], [2], [2], [1], [0]],
+};
 
-function getLevelIndexFromLocalStorage() {
-  const storedIndex = localStorage.getItem("currentLevelIndex");
-  if (storedIndex !== null) {
-    return parseInt(storedIndex, 10);
-  } else {
-    return 0; // Default level index if not found in localStorage
+const level2 = {
+  buttonCounters: [5, 3, 1],
+  values: [
+    [2, 3, 2, 2, 1],
+    [1, 2, 1, 3, 2],
+    [2, 2, 2, 1, 3],
+    [3, 1, 3, 2, 2],
+    [0, 2, 3, 1, 1],
+  ],
+};
+
+const level7 = {
+  buttonCounters: [7, 6, 3],
+  values: [
+    [3, 3, 2, 2, 6, 5],
+    [5, 1, 5, 1, 1, 1],
+    [1, 6, 6, 2, 2, 2],
+    [2, 2, 4, 4, 4, 4],
+    [6, 5, 1, 1, 6, 5],
+    [0, 4, 5, 6, 5, 4],
+  ],
+};
+
+const level9 = {
+  buttonCounters: [4, 3, 2],
+  values: [
+    [1, 1, 2, 1, 6],
+    [2, 1, 2, 2, 2],
+    [1, 2, 1, 1, 1],
+    [3, 3, 3, 3, 3],
+    [0, 1, 2, 1, 2],
+  ],
+};
+
+function generateLevel(
+  gridWidth,
+  gridHeight,
+  minButtonValue,
+  maxButtonValue,
+  buttonCounters
+) {
+  const level = {
+    buttonCounters: buttonCounters,
+    values: [],
+  };
+
+  for (let y = 0; y < gridHeight; y++) {
+    level.values[y] = [];
+    for (let x = 0; x < gridWidth; x++) {
+      if (x === 0 && y === gridHeight - 1) {
+        level.values[y][x] = 0;
+      } else {
+        level.values[y][x] =
+          Math.floor(Math.random() * (maxButtonValue - minButtonValue + 1)) +
+          minButtonValue;
+      }
+    }
   }
+
+  return level;
 }
 
+const level = level7;
+
 const gameState = {
-  currentLevelIndex: getLevelIndexFromLocalStorage(),
-  buttonCounters: [...levels[getLevelIndexFromLocalStorage()].buttonCounters],
-  playerIndex: {
-    x: 0,
-    y: levels[getLevelIndexFromLocalStorage()].values.length - 1,
-  },
-  values: levels[getLevelIndexFromLocalStorage()].values,
+  buttonCounters: [...level.buttonCounters],
+  playerIndex: { x: 0, y: level.values.length - 1 },
+  values: level.values,
   cells: [],
   selectCellBorder: null,
   selectedCell: null,
@@ -54,9 +93,8 @@ const gameState = {
   sumText: null,
   buttonTexts: [],
   moveButtonCounters: null,
-  gridWidth: levels[getLevelIndexFromLocalStorage()].values[0]?.length,
-  gridHeight: levels[getLevelIndexFromLocalStorage()].values.length,
-  levelTitleText: `Level: 0${getLevelIndexFromLocalStorage()}`,
+  gridWidth: level.values[0]?.length,
+  gridHeight: level.values.length,
 };
 
 var game = new Phaser.Game(config);
@@ -97,16 +135,6 @@ function create() {
   gameState.sumText = this.add
     .text(105, 460, "Sum: 0", { fontSize: "32px", fill: "#6bf2f2" })
     .setOrigin(0.5);
-
-  gameState.levelTitleText = this.add.text(
-    20,
-    20,
-    `Level: 0${gameState.currentLevelIndex + 1}`,
-    {
-      fontSize: "24px",
-      fill: "#ffffff",
-    }
-  );
 }
 
 function createGrid(gridWidth, gridHeight) {
@@ -244,17 +272,24 @@ function onButtonClick(i, buttonText) {
         sum >
         gameState.values[gameState.selectedCell.y][gameState.selectedCell.x]
       ) {
-        gameState.selectedNumbers = [];
-        sum = 0;
-        gameState.sumText.setText("Sum: " + sum);
-        gameState.buttonCounters = [...gameState.moveButtonCounters];
-        updateButtonText();
-        const warningContainer = createWarningContainer.call(this);
+        this.cameras.main.setBackgroundColor(0xffa07a);
+        var errorMessage = this.add
+          .text(400, 30, "Error: Sum exceeded!", {
+            fontSize: "32px",
+            fill: "#ff0000",
+          })
+          .setOrigin(0.5);
 
         this.time.delayedCall(
           500,
           function () {
-            warningContainer.destroy();
+            gameState.selectedNumbers = [];
+            sum = 0;
+            gameState.sumText.setText("Sum: " + sum);
+            gameState.buttonCounters = [...gameState.moveButtonCounters];
+            updateButtonText();
+            this.cameras.main.setBackgroundColor(0x000000);
+            errorMessage.destroy();
           },
           [],
           this
@@ -276,15 +311,13 @@ function movePlayer() {
   ].setFillStyle(0x9163bf);
 
   gameState.selectedCell = null;
-  if (gameState.selectCellBorder) {
-    gameState.selectCellBorder.destroy();
-  }
+  gameState.selectCellBorder.destroy();
 
   if (
     gameState.playerIndex.y === 0 &&
     gameState.playerIndex.x === gameState.gridWidth - 1
   ) {
-    showVictoryScreen.call(this);
+    alert("You win!");
   }
 }
 
@@ -294,9 +327,17 @@ function updateButtonText() {
   }
 }
 
-function resetLevel() {
-  loadLevel(levels[gameState.currentLevelIndex]);
-  this.scene.restart();
+function resetGame() {
+  gameState.buttonCounters = [...level.buttonCounters];
+  updateButtonText();
+
+  gameState.playerIndex = { x: 0, y: level.values.length - 1 };
+  gameState.values = level.values;
+
+  createGrid.call(this, gameState.gridWidth, gameState.gridHeight);
+
+  gameState.selectedNumbers = [];
+  gameState.sumText.setText("Sum: 0");
 }
 
 function createResetButton() {
@@ -311,134 +352,8 @@ function createResetButton() {
     .setScale(0.5); // Adjust the scale according to the size of your icon
 
   resetButton.on("pointerdown", () => {
-    resetLevel.call(this);
+    resetGame.call(this);
   });
-}
-
-function loadLevel(level) {
-  gameState.buttonCounters = [...level.buttonCounters];
-  gameState.values = level.values;
-  gameState.gridWidth = level.values[0]?.length;
-  gameState.gridHeight = level.values.length;
-  gameState.playerIndex = { x: 0, y: gameState.gridHeight - 1 };
-  gameState.selectedNumbers = [];
-  gameState.sumText = null;
-  gameState.selectCellBorder = null;
-  gameState.selectedCell = null;
-  gameState.buttonTexts = [];
-  gameState.levelTitleText = `Level: 0${gameState.currentLevelIndex}`;
-}
-
-function nextLevel() {
-  if (gameState.currentLevelIndex < levels.length - 1) {
-    updateLevelIndex(gameState.currentLevelIndex + 1);
-    loadLevel(levels[gameState.currentLevelIndex]);
-    this.scene.restart();
-  } else {
-    showGameWonMessage.call(this);
-  }
-}
-
-function showVictoryScreen() {
-  const centerX = this.sys.game.config.width / 2;
-  const centerY = this.sys.game.config.height / 2;
-
-  const victoryContainer = this.add.container(centerX, centerY).setDepth(1);
-
-  const victoryBackground = this.add.rectangle(0, 0, 300, 200, 0xffffff, 0.8);
-  victoryContainer.add(victoryBackground);
-
-  const victoryText = this.add.text(0, -50, "You Win!", {
-    fontSize: "32px",
-    fill: "#000000",
-  });
-  victoryText.setOrigin(0.5);
-  victoryContainer.add(victoryText);
-
-  const nextLevelButton = this.add.text(0, 20, "Next Level", {
-    fontSize: "24px",
-    fill: "#000000",
-  });
-  nextLevelButton.setOrigin(0.5);
-  victoryContainer.add(nextLevelButton);
-
-  const buttonBorder = this.add.rectangle(
-    0,
-    20,
-    nextLevelButton.width + 10,
-    nextLevelButton.height + 10
-  );
-  buttonBorder.setStrokeStyle(2, 0x000000);
-  buttonBorder.setOrigin(0.5);
-  victoryContainer.add(buttonBorder);
-
-  nextLevelButton.setInteractive();
-  nextLevelButton.on("pointerdown", () => {
-    victoryContainer.destroy();
-    nextLevel.call(this);
-  });
-}
-
-function showGameWonMessage() {
-  const centerX = this.sys.game.config.width / 2;
-  const centerY = this.sys.game.config.height / 2;
-
-  const container = this.add.container(centerX, centerY).setDepth(1);
-  const background = this.add.rectangle(0, 0, 550, 150, 0xffffff, 0.8);
-
-  const text = this.add
-    .text(0, -25, "Congratulations, you won the game!", {
-      fontSize: "24px",
-      color: "#000000",
-      align: "center",
-    })
-    .setOrigin(0.5);
-
-  const restartButton = this.add
-    .text(0, 25, "Restart Game", {
-      fontSize: "20px",
-      color: "#000000",
-      backgroundColor: "#ffffff",
-      padding: { left: 10, right: 10, top: 5, bottom: 5 },
-    })
-    .setOrigin(0.5);
-
-  restartButton.setInteractive({ useHandCursor: true });
-
-  container.add(background);
-  container.add(text);
-  container.add(restartButton);
-
-  restartButton.on("pointerdown", () => {
-    container.destroy();
-    updateLevelIndex(0);
-    loadLevel(levels[gameState.currentLevelIndex]);
-    this.scene.restart();
-  });
-}
-
-function createWarningContainer() {
-  const centerX = this.sys.game.config.width / 2;
-  const centerY = this.sys.game.config.height / 2;
-
-  const warningContainer = this.add.container(centerX, centerY).setDepth(1);
-  const background = this.add.rectangle(0, 0, 450, 100, 0xffa07a, 0.8);
-  const warningText = this.add
-    .text(0, 0, "Cell value exceeded!", {
-      fontSize: "32px",
-      fill: "#ff0000",
-    })
-    .setOrigin(0.5);
-
-  warningContainer.add(background);
-  warningContainer.add(warningText);
-
-  return warningContainer;
-}
-
-function updateLevelIndex(newIndex) {
-  gameState.currentLevelIndex = newIndex;
-  localStorage.setItem("currentLevelIndex", newIndex);
 }
 
 function update() {}
